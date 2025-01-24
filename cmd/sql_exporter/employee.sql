bb3749f5af0ff48221f8271c65f287e70c85bc97
hiteshbedre <32206192+hiteshbedre@users.noreply.github.com>
2025-01-24 11:41:59 +0530

    Update employee.sql

--- cmd/sql_exporter/employee.sql
+++ cmd/sql_exporter/employee.sql
@@ -14,5 +14,4 @@ INSERT INTO employees (id, first_name, last_name, email, hire_date, job_title, s
 (1, 'John', 'Doe', 'john.doe@example.com', '2020-01-15', 'Software Engineer', 75000.00),
 (2, 'Jane', 'Smith', 'jane.smith@example.com', '2019-03-22', 'Project Manager', 85000.00),
 (3, 'Michael', 'Brown', 'michael.brown@example.com', '2018-07-30', 'Data Analyst', 65000.00),
-(4, 'Emily', 'Davis', 'emily.davis@example.com', '2021-05-10', 'UX Designer', 70000.00),
 (5, 'David', 'Wilson', 'david.wilson@example.com', '2017-11-05', 'DevOps Engineer', 80000.00);
